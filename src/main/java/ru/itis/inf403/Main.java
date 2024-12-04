eb35baa449a798668d98c57264516870a3935f9d
Azaliya <safiullinaazalia79@gmail.com>
2024-12-04 12:24:07 +0300

    Update Main.java

--- src/main/java/ru/itis/inf403/Main.java
+++ src/main/java/ru/itis/inf403/Main.java
@@ -2,6 +2,6 @@ package ru.itis.inf403;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 11-403");
+        System.out.println("Hello");
     }
 }
